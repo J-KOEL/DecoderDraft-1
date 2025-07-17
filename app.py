9642b30f06f6e714b0065c6a80bf8fb5d4dbea32
J-KOEL <jaysonkoel05@gmail.com>
2025-07-17 10:16:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+from logic.decoder import decode_catalog_number
+
+st.title("10250T Catalog Decoder")
+
+catalog_number = st.text_input("Enter Catalog Number")
+
+if catalog_number:
+    result = decode_catalog_number(catalog_number)
+    st.write("Decoded Result:", result)
 
